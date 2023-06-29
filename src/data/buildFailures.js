be3b9371af2c78bec4f120d66697ce6ba6fa4578
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-06-28 19:44:39 -0700

    Create buildFailures.js

---
+++ src/data/buildFailures.js
@@ -0,0 +1,22 @@
+export const buildFailures = [
+  {
+    buildId: '1',
+    project: 'It seems my previous message was cut off. Lets continue:'
+
+**/src/data/buildFailures.js**
+
+export const buildFailures = [
+  {
+    buildId: '1',
+    project: 'Project 1',
+    timestamp: '2023-06-28 12:34',
+    errorMessage: 'Error 1',
+  },
+  {
+    buildId: '2',
+    project: 'Project 2',
+    timestamp: '2023-06-28 12:35',
+    errorMessage: 'Error 2',
+  },
+
+];
